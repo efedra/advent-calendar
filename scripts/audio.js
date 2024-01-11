c8942f7aefd6906a585aa4087b3c97a4f63214cf
efedra <efedra-00@mail.ru>
2024-01-11 20:34:52 +0300

    2

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,7 +1,7 @@
 
 const audioPlayer = document.querySelector(".audio-player");
 const audio = new Audio(
-    "../audio/RAMIN-SNOW.mp3"
+    "https://github.com/efedra/advent-calendar/blob/main/audio/RAMIN-SNOW.mp3"
 );
 
 audio.addEventListener("loadeddata", () => {
